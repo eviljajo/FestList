b1b5f600f7f8338b92a31d0bf30a3c70cf8af56d
eviljajo <eviljajo@gmail.com>
2019-03-27 19:25:01 +0100

    Delete button added

--- src/main/java/com/festlist/controllers/MainController.java
+++ src/main/java/com/festlist/controllers/MainController.java
@@ -1,9 +1,12 @@
 package com.festlist.controllers;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,13 +24,13 @@ public class MainController  {
 		this.festivalService = festivalService;
 	}
 	
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	@RequestMapping(value = "/addfest", method = RequestMethod.GET)
 	public String addFest(Model model) {
 		model.addAttribute("festival", new Festival());
 		return "addfest";
 	}
 	
-	@RequestMapping(value ="/festival", method = RequestMethod.GET)
+	@RequestMapping(value ="/", method = RequestMethod.GET)
 	public String festivalList(Model model) {
 		model.addAttribute("festival", festivalService.getAllFestivals());
 		return "festival";
@@ -40,6 +43,13 @@ public class MainController  {
 	return "festival";
 	}
 	
+	
+	@RequestMapping(value ="/delete/{id}", method = RequestMethod.GET)
+	public String deleteFestival(@PathVariable Long id, Model model) {
+		festivalService.deleteFestival(id);
+		return "festival";
+		
+	}
 	
 	
 
